7b2b1850f6f036694be1a97434870f29f3d0d3aa
Kenny <ksecond@pursuit.org>
2024-06-24 10:48:38 -0400

    updated server info

--- server.js
+++ server.js
@@ -4,35 +4,36 @@ const port = 8888;
 
 
 
-app.get('/:verb/:adjective/:noun', (req, res) => {
-    const { verb, adjective, noun } = req.params;
+app.get('/:verb/:adjective/:noun', (req, res) => { const { verb, adjective, noun } = req.params;
     const responseMessage = `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`;
     res.send(responseMessage);
 });
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>Welcome to the Bug Tracker</h1>
-        <a href="/bugs">View Bugs</a>
+app.get('/', (req, res) => { res.send(` <h1>Welcome to the Bug Tracker</h1>
+      
+    <a href="/bugs">View Bugs</a>
     `);
 });
 
 app.get('/bugs', (req, res) => {
-    res.send(`
-        <h1>99 little bugs in the code</h1>
+    //the initial 99 bugs text
+    res.send(`  <h1>99 little bugs in the code</h1>
         <a href="/bugs/101">Pull one down, patch it around</a>
     `);
 });
 
-app.get('/bugs/:numberOfBugs', (req, res) => {
-    const numberOfBugs = parseInt(req.params.numberOfBugs, 10);
+app.get('/bugs/:numberOfBugs', (req, res) => {  const numberOfBugs = parseInt(req.params.numberOfBugs, 10);
     if (numberOfBugs >= 200) {
-        res.send(`
-            <h1>${numberOfBugs} bugs in the code</h1>
+        res.send(`  <h1>${numberOfBugs} bugs in the code</h1>
             <a href="/bugs">Start over</a>
         `);
-    } else {
+    }
+     else 
+    
+    {
         const nextNumberOfBugs = numberOfBugs + 2;
+
+        //each time the number of bugs is added by 2 until it reaches 200.  Unless its counting down
         res.send(`
             <h1>${numberOfBugs} little bugs in the code</h1>
             <a href="/bugs/${nextNumberOfBugs}">Pull one down, patch it around</a>
@@ -40,9 +41,10 @@ app.get('/bugs/:numberOfBugs', (req, res) => {
     }
 });
 
-app.get('/:verb/:adjective/:noun', (req, res) => {
-    const { verb, adjective, noun } = req.params;
+app.get('/:verb/:adjective/:noun', (req, res) => {  const { verb, adjective, noun } = req.params;
     const projectName = `${verb}-${adjective}-${noun}`;
+
+    //the random project name
     res.send(`Congratulations on starting a new project called ${projectName}!`);
 });
 
